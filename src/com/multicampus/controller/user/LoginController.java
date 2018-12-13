20eb4e9a0bedc7e51ad139c6daee2ebe720fc272
JungYoungsu <terun707@gmail.com>
2018-12-13 14:48:05 +0900

    RequestMapping GET,POST

--- src/com/multicampus/controller/user/LoginController.java
+++ src/com/multicampus/controller/user/LoginController.java
@@ -3,6 +3,7 @@ package com.multicampus.controller.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.multicampus.biz.user.UserService;
 import com.multicampus.biz.user.UserVO;
@@ -12,12 +13,17 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping("/login.do")
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String loginView() throws Exception {
+		return "redirect:login.jsp";
+	}
+
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
 	public String login(UserVO vo) throws Exception {
 		if (userService.getUser(vo) != null)
 			return "redirect:getBoardList.do";
 		else
-			return "login";
+			return "redirect:login.jsp";
 	}
 
 }
