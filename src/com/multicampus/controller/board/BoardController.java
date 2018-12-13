f26b0fa934086c1af3f68676bcc23a3f18eed68b
JungYoungsu <terun707@gmail.com>
2018-12-13 15:57:16 +0900

    ModelData- Model

--- src/com/multicampus/controller/board/BoardController.java
+++ src/com/multicampus/controller/board/BoardController.java
@@ -1,12 +1,10 @@
 package com.multicampus.controller.board;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.multicampus.biz.board.BoardService;
@@ -46,8 +44,8 @@ public class BoardController {
 	}
 
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardVO vo, Map<String, List<BoardVO>> model) throws Exception {
-		model.put("boardList", boardService.getBoardList(vo));
+	public String getBoardList(BoardVO vo, Model model) throws Exception {
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList";
 	}
 
