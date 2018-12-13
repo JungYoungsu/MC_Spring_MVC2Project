fd8963841d513be2890d8f077e06575944b891f7
JungYoungsu <terun707@gmail.com>
2018-12-13 15:25:13 +0900

    Controller- session logout

--- src/com/multicampus/controller/user/LogoutController.java
+++ src/com/multicampus/controller/user/LogoutController.java
@@ -1,17 +1,17 @@
 package com.multicampus.controller.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-public class LogoutController implements Controller {
+@Controller
+public class LogoutController {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) throws Exception {
+		session.invalidate();
+		return "redirect:index.jsp";
 	}
 
 }
